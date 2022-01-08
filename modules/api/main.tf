24f2a8aeaa24684b07ae5113d3cff38f2c0a26ae
Emerson Valentim <emevalentim0@gmail.com>
2022-01-08 11:56:18 -0300

    feat: add logs and execution role policy on api module

--- modules/api/main.tf
+++ modules/api/main.tf
@@ -8,10 +8,30 @@ data "archive_file" "dummy" {
   }
 }
 
+
+module "cloudwatch" {
+  source = "../../resources/cloudwatch"
+
+  name = "${var.service}-api-${var.env}"
+}
+
 resource "aws_iam_role" "iam_for_lambda" {
   name = "${var.service}-api-${var.env}"
 
   assume_role_policy = file("${path.module}/lambda-policy.json")
+}
+
+resource "aws_iam_policy" "policy_for_lambda" {
+  name = "${var.service}-api-${var.env}"
+  path = "/"
+  policy = templatefile("${path.module}/lambda-policy.json.tpl", {
+    cloudwatch_log_group = module.cloudwatch.logs.arn
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.policy_for_lambda.arn
 }
 
 resource "aws_lambda_function" "api_lambda" {
