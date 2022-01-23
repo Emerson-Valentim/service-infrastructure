88550e899ccd9863788654195d6aca4cd3fb3a68
Emerson Valentim <emevalentim0@gmail.com>
2022-01-22 23:37:39 -0300

    feat: setup redis db for socket and worker

--- applications/storage/output.tf
+++ applications/storage/output.tf
@@ -1,3 +1,10 @@
 output "main-ecr" {
   value = aws_ecr_repository.main_ecr_storage
 }
+
+output "redis" {
+  value = {
+    worker = aws_elasticache_replication_group.worker-redis
+    socket = aws_elasticache_replication_group.socket-redis
+  }
+}
