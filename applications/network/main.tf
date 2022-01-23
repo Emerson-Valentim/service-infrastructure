e70fecafc44d1e30a3bbf861800131eb99d1e823
Emerson Valentim <emevalentim0@gmail.com>
2022-01-22 23:38:04 -0300

    feat: add network infra for redis

--- applications/network/main.tf
+++ applications/network/main.tf
@@ -4,9 +4,10 @@ module "main-vpc" {
   name = "main-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["${var.region}a", "${var.region}b", "${var.region}c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs                 = ["${var.region}a", "${var.region}b", "${var.region}c"]
+  private_subnets     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets      = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  elasticache_subnets = ["10.0.31.0/24", "10.0.32.0/24"]
 
   enable_ipv6        = true
   enable_nat_gateway = true
@@ -127,6 +128,52 @@ resource "aws_security_group" "kafka" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
+    cidr_blocks = module.main-vpc.private_subnets_cidr_blocks
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Environment = var.env
+  }
+}
+
+resource "aws_security_group" "worker-redis" {
+  name   = "worker-redis-${var.env}"
+  vpc_id = module.main-vpc.vpc_id
+
+  ingress {
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp"
+    cidr_blocks = module.main-vpc.private_subnets_cidr_blocks
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Environment = var.env
+  }
+}
+
+resource "aws_security_group" "socket-redis" {
+  name   = "socket-redis-${var.env}"
+  vpc_id = module.main-vpc.vpc_id
+
+  ingress {
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp"
     cidr_blocks = module.main-vpc.private_subnets_cidr_blocks
   }
 
